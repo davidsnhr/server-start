81244f22551e9462d58778cd3e3c2689dee70a03
David Sánchez <davidsanchez@Davids-MacBook-Pro-2.local>
2024-06-04 15:03:21 -0600

    add gemini api

--- models/descriptionModel.js
+++ models/descriptionModel.js
@@ -3,7 +3,7 @@ const { db } = require('../config/db');
 const getDescriptionById = async (id) => {
   try {
     const query =
-      'SELECT U.name, d.description FROM users U JOIN description d ON U.id = d.userd_id WHERE U.id = $1;';
+      'SELECT U.name, d.description, d.prescription, d.createat FROM users U JOIN description d ON U.id = d.userd_id WHERE U.id = $1;';
     const { rows } = await db.query(query, [id]);
     return rows;
   } catch (error) {
--- routes/chatOpenRoutes.js
+++ routes/chatOpenRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chatController');
+const geminiController = require('../controllers/geminiController');
 
 router.post('/', chatController.getResponseChat);
-
+router.post('/gemini', geminiController.getResponseChatGemini);
 
 module.exports = router;
-
-
